c07e47af61b2e645e70b4f3e27f23d90cd1d8a30
lamhien195 <lamhien2901@gmail.com>
2017-02-17 16:32:09 +0700

    HIEN-17022017

---
+++ ERP/ERP.Web/Areas/HopLong/Controllers/NghiepVuNguoiDungController.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Web.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP.Web.Areas.HopLong.Controllers
+{
+    public class NghiepVuNguoiDungController : Controller
+    {
+        private HOPLONG_DATABASEEntities db = new HOPLONG_DATABASEEntities();
+        // GET: HopLong/NghiepVuNguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
